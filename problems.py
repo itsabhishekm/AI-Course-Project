19b90d377437fab201dbcf94acb70c89cef057fc
itsabhishekm <mabhishek2817@gmail.com>
2026-03-27 18:59:07 -0700

    fully implemented problems.py file

--- problems.py
+++ problems.py
@@ -43,3 +43,52 @@ def get_neighbors(position):
 def manhattan_distance(pos1, pos2):
     
     return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+# Grid
+def print_grid(path, pickup=PICKUP, title=""):
+
+    grid = [['.' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+    for (r, c) in path:
+        grid[r][c] = '*'
+
+    #locations
+    sr, sc = START
+    pr, pc = pickup
+    dr, dc = DROPOFF
+    grid[sr][sc] = 'S'
+    grid[pr][pc] = 'P'
+    grid[dr][dc] = 'D'
+ 
+    # current position
+    if path:
+        rr, rc = path[-1]
+        grid[rr][rc] = 'R'
+ 
+    # grid printing
+    if title:
+        print(f"\n{'='*30}")
+        print(f"  {title}")
+        print(f"{'='*30}")
+ 
+    print("    " + "  ".join(str(c) for c in range(GRID_SIZE)))
+    print("   " + "---" * GRID_SIZE)
+    for r in range(GRID_SIZE):
+        row_display = "  ".join(grid[r])
+        print(f" {r} | {row_display}")
+ 
+    print()
+    print(f"  S = Start location of robot {START}")
+    print(f"  P = Pickup location of robot {pickup}")
+    print(f"  D = Dropoff location of robot {DROPOFF}")
+    print(f"  R = Current position of robot")
+    print(f"  * = Path taken to reach the destination")
+ 
+
+if __name__ == "__main__":
+    print("Grid size:", GRID_SIZE, "x", GRID_SIZE)
+    print("Start location:", START)
+    print("Pickup location:", PICKUP)
+    print("Dropoff location:", DROPOFF)
+    print("\nNeighbors of (0,0):", get_neighbors((0, 0)))
+    print("Manhattan distance from START to PICKUP:", manhattan_distance(START, PICKUP))
+    print_grid([START], title="Initial State")
